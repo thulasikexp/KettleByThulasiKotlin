92b400994dfee6803ddb85269aa5852e2b98bf00
Thulasi k <thulasik112001@gmail.com>
2025-02-17 10:52:24 +0530

    Added firebase to the app(crashlytics and analysis) Did firebase app distribution

--- app/src/main/java/com/thulsi/kettlebythulasi/MainActivity.kt
+++ app/src/main/java/com/thulsi/kettlebythulasi/MainActivity.kt
@@ -32,6 +32,7 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null)
                 .setAnchorView(R.id.fab).show()
+            throw RuntimeException("Test Crash")
         }
     }
 
